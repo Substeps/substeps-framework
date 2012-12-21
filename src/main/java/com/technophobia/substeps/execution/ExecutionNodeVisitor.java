c11e9461504c5b4be9302e1e8f0fe43cc5ef9338
Ricky Barefield <rbarefield@technophobia.com>
2012-12-21 15:09:57 +0000

    Added license headers

--- src/main/java/com/technophobia/substeps/execution/ExecutionNodeVisitor.java
+++ src/main/java/com/technophobia/substeps/execution/ExecutionNodeVisitor.java
@@ -1,3 +1,21 @@
+/*
+ *	Copyright Technophobia Ltd 2012
+ *
+ *   This file is part of Substeps.
+ *
+ *    Substeps is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    Substeps is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with Substeps.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.technophobia.substeps.execution;
 
 import com.technophobia.substeps.execution.node.BasicScenarioNode;
@@ -17,11 +35,11 @@ public interface ExecutionNodeVisitor<RETURN_TYPE> {
     RETURN_TYPE visit(BasicScenarioNode basicScenarioNode);
 
     RETURN_TYPE visit(OutlineScenarioNode outlineNode);
-    
+
     RETURN_TYPE visit(OutlineScenarioRowNode outlineScenarioRowNode);
 
     RETURN_TYPE visit(SubstepNode substepNode);
 
     RETURN_TYPE visit(StepImplementationNode stepImplementationNode);
-    
+
 }
